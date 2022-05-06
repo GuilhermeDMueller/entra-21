6863df0ea0f862a201098840379619d420778af8
suporte <suporte@X11>
2022-05-06 19:43:25 -0300

    Add project files.

---
+++ Entra21.WhileEA/Program.cs
@@ -0,0 +1,12 @@
+﻿using Entra21.WhileEA;
+
+Console.WriteLine(@"--------Menu--------
+1 | Exercicio1");
+Console.Write("Informe a ação desejada: ");
+int menu = Convert.ToInt32(Console.ReadLine());
+
+if (menu == 1)
+{
+    WhileExercicios1 newExercicio = new WhileExercicios1();
+    newExercicio.Executar();
+}
---
+++ Entra21.WhileEA/WhileExercicios1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.WhileEA
+{
+    internal class WhileExercicios1
+    {
+        public void Executar()
+        {
+            double peso = 0;
+            int quantidadePessoas = 0;
+
+            while ((peso > 0) || (peso < 300))
+            {
+                Console.WriteLine("Informe o seu peso: ");
+                peso = Convert.ToDouble(Console.ReadLine());
+
+                quantidadePessoas = quantidadePessoas + 1;
+            }
+            
+        }
+    }
+}
