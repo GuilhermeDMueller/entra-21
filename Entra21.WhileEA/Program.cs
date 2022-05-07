35417d5471f4fbc8e559a2c2e1ae122d9945397e
Guilherme Dzesigaleki Mueller <guilhermemuellerdzesigaleski@gmail.com>
2022-05-06 22:09:42 -0300

    Continuação da atividade de while (Adicionando e resolvendo as atividades.)

--- Entra21.WhileEA/Program.cs
+++ Entra21.WhileEA/Program.cs
@@ -1,7 +1,8 @@
 ﻿using Entra21.WhileEA;
 
 Console.WriteLine(@"--------Menu--------
-1 | Exercicio1");
+1 | Exercicio1
+2 | Exercicio2");
 Console.Write("Informe a ação desejada: ");
 int menu = Convert.ToInt32(Console.ReadLine());
 
@@ -9,4 +10,9 @@ if (menu == 1)
 {
     WhileExercicios1 newExercicio = new WhileExercicios1();
     newExercicio.Executar();
+}
+else if (menu == 2)
+{
+    WhileExercicios2 bestExercicio = new WhileExercicios2();
+    bestExercicio.Executar();
 }
---
+++ Entra21.WhileEA/WhileExercicios2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.WhileEA
+{
+    internal class WhileExercicios2
+    {
+        public void Executar()
+        {
+            int indice = 0;
+            
+            Console.Write("Digite a quantidade de carros desejada:  ");
+            int quantidadeCarros = Convert.ToInt32(Console.ReadLine());          
+            int mediaAno = 0;
+            double mediaValor = 0;
+            indice = indice + 1;
+
+
+            while (indice < quantidadeCarros)
+            {
+                Console.Write("Informe o modelo do carro: ");
+                string modeloCarro = Console.ReadLine();
+
+                Console.Write("Informe o valor do carro: ");
+                double valorCarro = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Informe o ano do carro: ");
+                int anoCarro = Convert.ToInt32(Console.ReadLine());
+
+                int somaAno = anoCarro * quantidadeCarros;
+                int mediaAno = somaAno / indice;
+
+                double somaValor = valorCarro * quantidadeCarros;
+                double mediaValor = somaValor / indice;
+                
+
+            }
+            Console.WriteLine("A media de ano dos carros é :" + mediaAno);
+
+
+        }
+
+    }
+}
