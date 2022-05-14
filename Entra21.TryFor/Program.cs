af6ae14df8e0407bca59c2220917d578074f2fad
Ana Paula Wruck <wruck.ap@gmail.com>
2022-05-13 21:53:06 -0300

    Inicio de FOR Primeira atividade iniciada

---
+++ Entra21.TryFor/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using Entra21.TryFor;
+
+Console.WriteLine(@"-----------Menu----------
+1 | Exercício01
+");
+Console.Write("Informe a ação desejada: ");
+int acao = Convert.ToInt32(Console.ReadLine());
+
+if (acao == 1)
+{
+    TryForExercicio01 newExercicio = new TryForExercicio01();
+    newExercicio.Executar();
+}
+
---
+++ Entra21.TryFor/TryForExercicio01.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.TryFor
+{
+    internal class TryForExercicio01
+    {
+        public void Executar()
+        {
+            int indice = 0;
+
+            while (indice < 13)
+            {
+                Console.WriteLine("Informe o nome da peça: ");
+                string nome = Console.ReadLine();
+
+                Console.WriteLine("Informe o valor da peça: ");
+                double valorPeca = Convert.ToDouble(Console.ReadLine());
+
+            }
+        }
+    }
+}
